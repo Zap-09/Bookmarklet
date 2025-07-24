09e70dde3003d2cdb2e3409df16599242950cafc
Zap <177562262+Zap-09@users.noreply.github.com>
2025-07-24 17:02:45 +0600

    Fixed the copy buttons and added a testing page

--- scripts/main.js
+++ scripts/main.js
@@ -1,46 +1,15 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const copyButton = document.getElementById('copyButton');
-    const codeElement = document.querySelector('code[data-code="booklet"]');
-  
-    copyButton.addEventListener('click', async function () {
-      const codeText = codeElement?.textContent?.trim();
-      if (!codeText) return;
-  
-      try {
-        await navigator.clipboard.writeText(codeText);
-        copyButton.classList.add('copied');
-        copyButton.textContent = "Copied!";
-        
-        setTimeout(() => {
-          copyButton.classList.remove('copied');
-          copyButton.textContent = "Copy";
-        }, 1500);
-      } catch (err) {
-        console.error("Copy failed:", err);
-      }
-    });
-  });
-  
+function copyText(section,buttonEle){
+    let toCopy = document.getElementById(section);
+    let text = toCopy.textContent.trim();
 
-  document.addEventListener("DOMContentLoaded", function () {
-    const copyButton = document.getElementById('copyButton2');
-    const codeElement = document.querySelector('pre[data-code="code"]');
-  
-    copyButton.addEventListener('click', async function () {
-      const codeText = codeElement?.textContent?.trim();
-      if (!codeText) return;
-  
-      try {
-        await navigator.clipboard.writeText(codeText);
-        copyButton.classList.add('copied');
-        copyButton.textContent = "Copied!";
-        
+    let button = document.getElementById(buttonEle)
+    
+    navigator.clipboard.writeText(text).then(() => {
+        button.classList.add("copied");
+        button.textContent = "Copied!";
         setTimeout(() => {
-          copyButton.classList.remove('copied');
-          copyButton.textContent = "Copy";
+            button.classList.remove("copied");
+            button.textContent = "Copy"
         }, 1500);
-      } catch (err) {
-        console.error("Copy failed:", err);
-      }
-    });
-  });
+    })
+}
